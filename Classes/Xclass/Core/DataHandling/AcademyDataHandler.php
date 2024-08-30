214a1009e75915a3bfae310fba5f4ba20af57159
Frodo Podschwadek <frodo.podschwadek@gmail.com>
2024-08-30 15:18:47 +0100

    Access Backend User via $GLOBALS instead as property (which is flagged as deprecated). @see https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.4/Deprecation-100454-LegacyTreeImplementations.html
    chore: update framework-related PHP code to 12.4 standards

--- Classes/Xclass/Core/DataHandling/AcademyDataHandler.php
+++ Classes/Xclass/Core/DataHandling/AcademyDataHandler.php
@@ -15,6 +15,7 @@
 
 namespace Digicademy\Academy\Xclass\Core\DataHandling;
 
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Database\RelationHandler;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
@@ -22,6 +23,10 @@ use TYPO3\CMS\Core\Utility\MathUtility;
 
 class AcademyDataHandler extends DataHandler
 {
+    // Get the backend user
+    /** @var BackendUserAuthentication $backendUser */
+    $backendUser = $GLOBALS['BE_USER'];
+
     /**
      * Processes child records in an inline (IRRE) element when the parent record is copied.
      *
@@ -82,7 +87,7 @@ class AcademyDataHandler extends DataHandler
                 if (!MathUtility::canBeInterpretedAsInteger($realDestPid)) {
                     $newId = $this->copyRecord($v['table'], $v['id'], -$v['id']);
                     // If the destination page id is a NEW string, keep it on the same page
-                } elseif ($this->BE_USER->workspace > 0 && BackendUtility::isTableWorkspaceEnabled($v['table'])) {
+                } elseif ($backendUser && $backendUser->workspace > 0 && BackendUtility::isTableWorkspaceEnabled($v['table'])) {
                     // A filled $workspaceOptions indicated that this call
                     // has it's origin in previous versionizeRecord() processing
                     if (!empty($workspaceOptions)) {
@@ -92,7 +97,7 @@ class AcademyDataHandler extends DataHandler
                         $this->versionizeRecord(
                             $v['table'],
                             $v['id'],
-                            $workspaceOptions['label'] ?? 'Auto-created for WS #' . $this->BE_USER->workspace,
+                            $workspaceOptions['label'] ?? 'Auto-created for WS #' . $backendUser->workspace,
                             $workspaceOptions['delete'] ?? false
                         );
                         // Otherwise just use plain copyRecord() to create placeholders etc.
@@ -105,7 +110,7 @@ class AcademyDataHandler extends DataHandler
                             $newId = $this->copyRecord($v['table'], $v['id'], $realDestPid);
                         }
                     }
-                } elseif ($this->BE_USER->workspace > 0 && !BackendUtility::isTableWorkspaceEnabled($v['table'])) {
+                } elseif ($backendUser && $backendUser->workspace > 0 && !BackendUtility::isTableWorkspaceEnabled($v['table'])) {
                     // We are in workspace context creating a new parent version and have a child table
                     // that is not workspace aware. We don't do anything with this child.
                     continue;
